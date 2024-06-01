3ffcf34bc88d65101d2327fc5eec02281b72db6b
cuongnvtse160875@gmail.com <cuongnvtse160875@gmail.com>
2024-06-01 07:31:15 +0700

    f

--- src/services/backend/constants.backend.ts
+++ src/services/backend/constants.backend.ts
@@ -1,8 +1,4 @@
-import { appConfig } from "@config"
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 
-const BACKEND_DEVELOPMENT_URL = process.env.NEXT_PUBLIC_BACKEND_DEVELOPMENT_URL
-const BACKEND_PRODUCTION_URL = process.env.NEXT_PUBLIC_BACKEND_PRODUCTION_URL
-
-export const backendUrl = () =>
-    appConfig.inProduction ? BACKEND_PRODUCTION_URL : BACKEND_DEVELOPMENT_URL
+export const backendUrl = () => BACKEND_URL
  
