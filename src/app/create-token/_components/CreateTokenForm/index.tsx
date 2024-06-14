7615d3fba4c2b6f870ef14b10dae1178f797ecfd
starci183 <cuongnvtse160875@gmail.com>
2024-06-14 17:42:40 +0700

    123123

--- src/app/create-token/_components/CreateTokenForm/index.tsx
+++ src/app/create-token/_components/CreateTokenForm/index.tsx
@@ -25,9 +25,12 @@ const WrappedCreateTokenForm = () => {
 
     return (
         <>
-            <Card className="max-w-[500px] mx-auto mt-12 border border-divider" shadow="none">
+            <Card
+                className="max-w-[500px] mx-auto mt-12 border border-divider"
+                shadow="none"
+            >
                 <CardHeader className="font-bold p-4 pb-0">
-                Create Token
+                    Create Token
                 </CardHeader>
                 <CardBody className="grid gap-4 p-4">
                     <Input
@@ -39,7 +42,9 @@ const WrappedCreateTokenForm = () => {
                         placeholder="USD Tether"
                         labelPlacement="outside"
                         label="Name"
-                        isInvalid={!!(formik.touched.name && formik.errors.name)}
+                        isInvalid={
+                            !!(formik.touched.name && formik.errors.name)
+                        }
                         errorMessage={formik.touched.name && formik.errors.name}
                     />
                     <Input
@@ -51,8 +56,12 @@ const WrappedCreateTokenForm = () => {
                         placeholder="USDT"
                         labelPlacement="outside"
                         label="Symbol"
-                        isInvalid={!!(formik.touched.symbol && formik.errors.symbol)}
-                        errorMessage={formik.touched.symbol && formik.errors.symbol}
+                        isInvalid={
+                            !!(formik.touched.symbol && formik.errors.symbol)
+                        }
+                        errorMessage={
+                            formik.touched.symbol && formik.errors.symbol
+                        }
                     />
                     <Input
                         id="decimals"
@@ -63,8 +72,15 @@ const WrappedCreateTokenForm = () => {
                         placeholder="6"
                         labelPlacement="outside"
                         label="Decimals"
-                        isInvalid={!!(formik.touched.decimals && formik.errors.decimals)}
-                        errorMessage={formik.touched.decimals && formik.errors.decimals}
+                        isInvalid={
+                            !!(
+                                formik.touched.decimals &&
+                                formik.errors.decimals
+                            )
+                        }
+                        errorMessage={
+                            formik.touched.decimals && formik.errors.decimals
+                        }
                     />
                     <Textarea
                         id="description"
@@ -74,8 +90,16 @@ const WrappedCreateTokenForm = () => {
                         placeholder=""
                         labelPlacement="outside"
                         label="Description"
-                        isInvalid={!!(formik.touched.description && formik.errors.description)}
-                        errorMessage={formik.touched.description && formik.errors.description}
+                        isInvalid={
+                            !!(
+                                formik.touched.description &&
+                                formik.errors.description
+                            )
+                        }
+                        errorMessage={
+                            formik.touched.description &&
+                            formik.errors.description
+                        }
                     />
                     <div className="grid gap-2">
                         <div className="text-sm">Icon Url</div>
@@ -102,7 +126,8 @@ const WrappedCreateTokenForm = () => {
                                                 )}...`
                                             ) : (
                                                 <span className="text-foreground-500">
-                                                Drop your file here or upload
+                                                    Drop your file here or
+                                                    upload
                                                 </span>
                                             )}
                                         </div>
@@ -110,30 +135,30 @@ const WrappedCreateTokenForm = () => {
                                 </section>
                             )}
                         </Dropzone>
-                        {
-                            !iconUrlSwrMutation.isMutating ? (
+                        {!iconUrlSwrMutation.isMutating ? (
+                            formik.values.iconUrl ? (
                                 <Link
                                     showAnchorIcon
                                     isExternal
                                     href={formik.values.iconUrl}
                                     size="sm"
                                 >
-                                View
+                                    View
                                 </Link>
-                            ) : (
-                                <Spinner
-                                    className="flex flex-row w-fit"
-                                    classNames={{
-                                        wrapper: "w-3.5 h-3.5",
-                                        circle1: "w-3.5 h-3.5",
-                                        circle2: "w-3.5 h-3.5",
-                                        label: "text-sm",
-                                    }}
-                                >
+                            ) : null
+                        ) : (
+                            <Spinner
+                                className="flex flex-row w-fit"
+                                classNames={{
+                                    wrapper: "w-3.5 h-3.5",
+                                    circle1: "w-3.5 h-3.5",
+                                    circle2: "w-3.5 h-3.5",
+                                    label: "text-sm",
+                                }}
+                            >
                                 Loading
-                                </Spinner>
-                            )
-                        }
+                            </Spinner>
+                        )}
                     </div>
                     <Input
                         id="totalSupply"
@@ -144,17 +169,25 @@ const WrappedCreateTokenForm = () => {
                         placeholder="10000000000"
                         labelPlacement="outside"
                         label="Total Supply"
-                        isInvalid={!!(formik.touched.totalSupply && formik.errors.totalSupply)}
-                        errorMessage={formik.touched.totalSupply && formik.errors.totalSupply}
+                        isInvalid={
+                            !!(
+                                formik.touched.totalSupply &&
+                                formik.errors.totalSupply
+                            )
+                        }
+                        errorMessage={
+                            formik.touched.totalSupply &&
+                            formik.errors.totalSupply
+                        }
                     />
                 </CardBody>
                 <CardFooter className="p-4 pt-0">
                     <Button type="submit" color="primary" className="w-full">
-                    Create
+                        Create
                     </Button>
                 </CardFooter>
             </Card>
-            <TokenCreatedSuccesfullyModal/>
+            <TokenCreatedSuccesfullyModal />
         </>
     )
 }
